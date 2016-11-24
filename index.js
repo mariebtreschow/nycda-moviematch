25ef01f20416882fae1eda1c3791fab62fbb7e3c
Marie Treschow <treschow.marie@gmail.com>
2016-11-24 16:18:39 +0100

    setting upp index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express'),
+      pug = require('pug'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      displayRoutes = require('express-routemap'),
+      session = require('express-session'),
+      bcrypt = require('bcrypt'),
+      morgan = require('morgan');
+
+var app = express(),
+    db = require('./models');
+
+var userRouter = require('./routes/user');
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(methodOverride((req, res) => {
+   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      var method = req.body._method;
+
+      delete req.body._method;
+      return method;
+   }})
+);
+
+app.use(session({
+   secret: 'secret key',
+   resave: true,
+   saveUninitialized: true
+ }));
+
+app.set('view engine', 'pug');
+
+app.get('/', userRouter);
