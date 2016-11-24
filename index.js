c2fa07cd644179b3dbea6415e967ee7af6801734
Isis van der Plas <isisvanderplas@hotmail.com>
2016-11-24 18:00:26 +0100

    initial styling homepage

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const express = require('express'),
       pug = require('pug'),
-      bodyParser = require('body-parser'),
-      methodOverride = require('method-override'),
-      displayRoutes = require('express-routemap'),
-      session = require('express-session'),
-      bcrypt = require('bcrypt'),
+      // bodyParser = require('body-parser'),
+      // methodOverride = require('method-override'),
+      // displayRoutes = require('express-routemap'),
+      // session = require('express-session'),
+      // bcrypt = require('bcrypt'),
       morgan = require('morgan');
 
-var app = express(),
-    db = require('./models');
+var app = express();
+    // db = require('./models');
 
-var userRouter = require('./routes/user');
+// var userRouter = require('./routes/user');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false}));
+// app.use(bodyParser.urlencoded({ extended: false}));
+//
+// app.use(methodOverride((req, res) => {
+//    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//       var method = req.body._method;
+//
+//       delete req.body._method;
+//       return method;
+//    }})
+// );
 
-app.use(methodOverride((req, res) => {
-   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      var method = req.body._method;
-
-      delete req.body._method;
-      return method;
-   }})
-);
-
-app.use(session({
-   secret: 'secret key',
-   resave: true,
-   saveUninitialized: true
- }));
+// app.use(session({
+//    secret: 'secret key',
+//    resave: true,
+//    saveUninitialized: true
+//  }));
 
 app.set('view engine', 'pug');
 
-app.get('/', userRouter);
+// app.get('/', userRouter);
+
+app.get('/', (req,res) => {
+  res.render('homepage');
+});
 
 app.listen(3000, (req, res) => {
    console.log('App listening on 3000!');
