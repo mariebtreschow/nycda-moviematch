b7c2467f6cbb2c9f73479e587f547895ba5e0552
Kevin Fung <kevinfung87@gmail.com>
2016-11-27 23:07:28 +0100

    delete movies

--- routes/admin.js
+++ routes/admin.js
@@ -16,29 +16,33 @@ router.get('/admin/movies', (request,response) => {
   response.render('admin/show');
 });
 
-
-module.exports = router;
-
-
-
-
-
-
-
-
-
+router.post('/admin/movies/new', (request, response) => {
+  if (request.body.title) {
+    MovieMatch.create(request.body).then(() => {
+      response.redirect('/');
+    });
+  } else {
+    reponse.redirect('/admin/movies');
+  }
+});
 
 
 
+router.delete('/admin/:id', (request, response) => {
+  MovieMatch.destroy({
+    where: {
+      id: request.params.id
+    }
+  }).then(() => {
+    response.redirect('admin/show');
+  });
+});
 
 
-
-
-
-
-
-
-
+router.get('/logout', (request, response) => {
+  request.session.user = undefined;
+  response.redirect('/');
+});
 
 
 module.exports = router;
