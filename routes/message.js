410216046d797a1b7b5ab51a3061a0d890e6f2f0
Marie Treschow <treschow.marie@gmail.com>
2016-12-13 17:24:58 +0100

    matching works

--- routes/message.js
+++ routes/message.js
@@ -5,13 +5,41 @@ const express = require('express'),
 
 router.get('/messages', (req, res) => {
    db.UserMatchRequest.findAll({
-      where: {
-         targetId: req.session.user.id
-      } // or this will be requestId
-   }).then
-});
+     where: {
+       requestId: {
+         $in: [
+           db.sequelize.literal(`SELECT "targetId" FROM "UserMatchRequests" WHERE "requestId" = ${req.session.user.id}`)
+         ]
+       }
+     }
+  }).then((matches) => {
+     var idsOfTheMatches = matches.map((match) => {
+        if (req.session.id === match.requestId) {
+           return match.targetId;
+        } else {
+           return match.requestId;
+        }
+     });
 
-   res.render('users/message', { user: req.session.user });
+     db.User.findAll({
+        where: {
+           id: {
+             $in: idsOfTheMatches
+          }
+        }
+     }).then((matchedUsers) => {
+        res.render('users/message', { user: req.session.user, matchedUsers: matchedUsers });
+     });
+  });
+
+   // db.UserMatchRequest.findAll({
+   //    where: {
+   //       targetId: req.session.user.id
+   //    } // or this will be requestId
+   // }).then((matchesWhereUserIsTarget) => {
+   //
+   // })
+
 });
 
 
